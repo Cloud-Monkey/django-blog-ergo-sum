41c1684be756e68b45bc9df00c77e0f4855815c9
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-09-04 07:44:19 +0000

    edit setting debug to true for detailed error messages

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5qa%@ts^9^u6&^s#&+snchqvr9d@!5%4krtr-rp1tes&=l+6a!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-cloudmonkey-djangobloge-jm17024io9w.ws.codeinstitute-ide.net', '.herokuapp.com']
 
